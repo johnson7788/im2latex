f49d290307dbe33f373971d6b46ea6e2a3f955f2
zhicai.guo <zhicai.guo@lavector.com>
2020-12-02 13:02:31 +0800

    2020120213

---
+++ download_data.sh
@@ -0,0 +1,7 @@
+cd data
+wget http://lstm.seas.harvard.edu/latex/data/im2latex_validate_filter.lst
+wget http://lstm.seas.harvard.edu/latex/data/im2latex_train_filter.lst
+wget http://lstm.seas.harvard.edu/latex/data/im2latex_test_filter.lst
+wget http://lstm.seas.harvard.edu/latex/data/formula_images_processed.tar.gz
+wget http://lstm.seas.harvard.edu/latex/data/im2latex_formulas.norm.lst
+tar -zxvf formula_images_processed.tar.gz
